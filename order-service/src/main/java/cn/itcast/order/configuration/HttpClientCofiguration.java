c91300eba322d8fa573da97e5b0e0f7bfb58535f
hfrangon <109893156+hfrangon@users.noreply.github.com>
2024-08-09 20:49:56 +0800

    分布式项目 Eureka注册服务和发现

---
+++ order-service/src/main/java/cn/itcast/order/configuration/HttpClientCofiguration.java
@@ -0,0 +1,15 @@
+package cn.itcast.order.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class HttpClientCofiguration {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
+
